cfe391034e9b2c06a08969e2019f600bfebceb09
秦振坤 <johnqin@rektec.com.cn>
2019-09-05 13:33:26 +0800

    首页demo展示

--- src/components/demoList.vue
+++ src/components/demoList.vue
@@ -1,7 +1,51 @@
 <template>
   <ul class="demo-list">
-    <li class="list-item">
-
+    <li class="list-item"
+      @click="chooseItem(item)"
+      v-for="(item, index) in demoList" :key="index"
+    >
+      <h2 class="title">{{ item.title }}</h2>
+      <p class="desc">{{ item.desc }}</p>
     </li>
   </ul>
 </template>
+<script>
+export default {
+  name: 'demo-list',
+  data () {
+    return {
+      demoList: [
+        { title: '歌手列表', desc: '字母导航，图片懒加载', path: '/singer' },
+        { title: '城市列表', desc: '城市信息展示, 字母导航', path: '/city' }
+      ]
+    }
+  },
+  methods: {
+    chooseItem (item) {
+      this.$router.push({
+        path: item.path
+      })
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.demo-list {
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  .list-item {
+    padding: 20px 10px;
+    border-bottom: 1px solid #f0f0f0;
+    line-height: 20px;
+    cursor: pointer;
+    .title {
+      font-size: 18px;
+      line-height: 30px;
+    }
+    .desc {
+      font-size: 14px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,31 @@
 <template>
   <div class="home">
-    
+    <h2 class="title">案例信息</h2>
+    <demo-list></demo-list>
   </div>
 </template>
 
 <script>
+import demoList from '../components/demoList'
+
 export default {
   name: 'home',
   data () {
     return {
-      
+
     }
+  },
+  components: {
+    demoList
   }
 }
 </script>
+<style lang="less" scoped>
+.home {
+  .title {
+    padding: 20px;
+    line-height: 36px;
+    text-align: center;
+  }
+}
+</style>

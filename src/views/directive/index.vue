fc16b8b3019d0d4ee014fd6cfa119cb14c608fd3
秦振坤 <johnqin@rektec.com.cn>
2019-09-16 17:31:46 +0800

    滑动验证图组件

--- src/views/directive/index.vue
+++ src/views/directive/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="directive">
-    <textarea v-model="copyText" class="content"></textarea>
-    <button v-copy="copyText">复制</button>
+    <!-- <textarea v-model="copyText" class="content"></textarea>
+    <button v-copy="copyText">复制</button> -->
+    <slider-picture></slider-picture>
   </div>
 </template>
 <script>
+import sliderPicture from '../../components/slidingPicture'
+
 export default {
   data () {
     return {
       copyText: ''
     }
+  },
+  components: {
+    sliderPicture
   }
 }
 </script>
@@ -18,4 +24,10 @@ export default {
   width: 100%;
   height: 36px;
 }
+// .directive {
+//   position: relative;
+//   width: 300px;
+//   padding: 0;
+//   border-radius: 2px;
+// }
 </style>
